7c496d96b57cdb1c98011b04486ad4a608375d12
bbsekiya <brian210003@gmail.com>
2016-12-31 13:03:25 -0800

    deduc1

---
+++ new1/deduc1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+
+template <typename T>
+struct Data {
+	T		m_Data1;
+	T		m_Data2;
+	
+	Data(T t1, T t2) : m_Data1(t1), m_Data2(t2) {}
+	
+	Data(const Data& that) : m_Data1(that.m_Data1), m_Data2(that.m_Data2) {
+		std::cout << "....Data copy ...\n";
+	}
+	
+	Data(Data&& that) : m_Data1(std::move(that.m_Data1)), m_Data2(std::move(that.m_Data2)) {
+		that.m_Data1 = 0;
+		that.m_Data2 = 0;
+		std::cout << ".... Data move ....\n";
+	}
+	
+};
+
+template <typename T>
+auto&& func(T&& t) {
+//T&& func(T&& t) {
+	return std::forward<T>(t);
+}
+
+
+template <typename T>
+auto&& func1(T&& t) {
+//T&& func(T&& t) {
+	return std::forward<T>(t).m_Data1;
+}
+
+
+int main()
+{
+	Data<int>	d(100, 200);
+	
+
+	
+	Data<int> d2 = func(std::move(d)) ;
+	std::cout << "d2.m_Data1 = " << d2.m_Data1 << std::endl;
+	
+	std::cout << "\n ----------------------------\n";
+	
+	Data<int>	d3(333, 444);
+
+	int y1 = func1(d3);
+	std::cout << "y1 = " << y1 << std::endl;
+	
+
+
+	return 0;
+}
