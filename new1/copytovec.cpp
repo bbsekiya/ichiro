edba3fed367b2354b32e2ad2bd4e94bc7295dc9b
bbsekiya <brian210003@gmail.com>
2016-12-31 11:35:53 -0800

    update

--- new1/copytovec.cpp
+++ new1/copytovec.cpp
@@ -1,5 +1,24 @@
-
-
+#include <iostream>
+#include <vector>
 
 
 // td::vector<ValueType> vec(a, a + n);
+
+
+
+int main()
+{
+	char source[] = "1234567890";
+	char *mem = &source[0];
+	
+	std::cout << "*mem = " << *mem << std::endl;
+	
+	std::vector<uint8_t> vec(mem, mem+10);
+	
+	for (auto it=vec.cbegin(); it!=vec.cend(); it++) {
+			std::cout << *it << std::endl;
+	}
+	
+	return 0;
+}
+	
