266972d3cae711b560081432c3e915c6725a32ab
bbsekiya <brian210003@gmail.com>
2017-02-10 16:35:02 -0800

    update

---
+++ new1/dataptr.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+class Data {
+
+};
+
+namespace data {
+
+class DataSetter {
+	public:
+		virtual ~DataSetter() {}
+		virtual void set(boost::shared_ptr<const Data>& p)  = 0;
+};		
+			
+
+namespace version2 {
+	class DataSetter : public data::DataSetter
+	{
+		public:
+			DataSetter() {}
+			virtual ~DataSetter() {}
+			void set(boost::shared_ptr<const Data>& p) override {
+				std::cout << "------ version2 : set ----------\n";
+			}
+	};
+
+}
+
+namespace version3 {
+	class DataSetter : public data::DataSetter
+	{
+		public:
+			DataSetter() {}
+			virtual ~DataSetter() {}
+			void set(boost::shared_ptr<const Data>& p) override {
+				std::cout << "------ version2 : set ----------\n";
+			}
+	};
+
+}
+}
+
+
+
+class ConvertData
+{
+	public:
+		ConvertData(const uint32_t n=3) {
+			if (n == 2) {
+				dataSetter = boost::make_shared<data::version2::DataSetter>();
+			} else {
+				dataSetter = boost::make_shared<data::version3::DataSetter>();
+			}
+			
+		}
+		virtual ~ConvertData() {}
+		
+		void put(boost::shared_ptr<const Data>& p) {
+			dataSetter->set(p);
+		}
+		
+	private:
+		boost::shared_ptr<data::DataSetter>	dataSetter;
+};
+
+
+
+int main()
+{
+	ConvertData		d1;
+	boost::shared_ptr<const Data>	p = boost::make_shared<Data>();
+	
+	
+	d1.put(p);
+	
+	ConvertData d3(3);
+	
+	d3.put(p);
+	
+	
+}
+
+
+
+
+	
